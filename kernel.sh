b5f612dcb71af9f74dea2d9510149a1811b803c4
Vijay Malav <jaymalav10@gmail.com>
2022-03-21 23:00:04 +0530

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -189,7 +189,7 @@ DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning Clang-14 ||"
-		git clone --depth=1 https://github.com/vijaymalav564/vortex-clang clang-llvm
+		git clone --depth=1 https://gitlab.com/Vijaymalav564/vortex-clang.git clang-llvm
 		# Toolchain Directory defaults to clang-llvm
 		TC_DIR=$KERNEL_DIR/clang-llvm
 	fi
